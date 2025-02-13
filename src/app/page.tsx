7cfb06ef767517611636bc253df1b1e75c94a6fb
Rickster42 <rickytaylor42@virginmedia.com>
2025-02-13 02:41:05 +0000

    Added data syncing with Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ThemeToggle from "@/components/shared/theme-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
     <div className="p-5">
-      <div className="w-100 flex justify-end">
+      <div className="w-100 flex gap-x-5 justify-end">
+        <UserButton />
         <ThemeToggle />
       </div>
       <h1 className=" text-blue-500">Welcome to the home page</h1>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
  
-const isProtectedRoute = createRouteMatcher(["/dashboard",'/dashboard(.*)'])
+const protectedRoutes = createRouteMatcher(['/'])
  
-export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
+export default clerkMiddleware(async (auth, request) => {
+  if (protectedRoutes(request)) {
+    await auth.protect()
+  }
 })
  
 export const config = {
@@ -12,7 +14,7 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-    // Exclude `/api/webhooks` from authentication
     '/((?!api/webhooks).*)',
+
   ],
-}
+};
