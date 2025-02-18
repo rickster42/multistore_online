d06bf2e52c4829cceabf11768286cd75d45f859c
rickster42 <rickytaylor42@virginmedia.com>
2025-02-18 14:53:29 +0000

    Added basic Clerk authentication

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Barlow } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider } from '@clerk/nextjs'
 
 // Fonts
 const interFont = Inter({ subsets: ["latin"] });
@@ -23,6 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+  <ClerkProvider>
     <html lang="en" suppressHydrationWarning={true}>
       <body className={`${interFont.className} ${barlowFont.variable}`}>
         <ThemeProvider
@@ -35,5 +37,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+  </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import ThemeToggle from "@/components/ui/shared/theme-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="p-5">
       <div className="w-100 flex gap-x-5 justify-end">
+      <UserButton />
         <ThemeToggle />
       </div>
-      <h1 className="fon-bold text-blue-500 font-barlow">Welcome to the home page!</h1>
+      <h1 className=" text-blue-500">Welcome to the home page!</h1>
     </div>
   );
 }
